e448e310820e9d855d2703a86f688393d89d531b
mcmullenhjack <60825266+mcmullenhjack@users.noreply.github.com>
2020-06-05 09:39:12 -0400

    edited some code

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=devi
 server = app.server
 app.title='The Golden Age of Television'
 
-image_filename = '/Users/jackmcmullen/First_dashboard_stuff/assets/hackcville-logo.png'
+# image_filename = '/Users/jackmcmullen/First_dashboard_stuff/assets/hackcville-logo.png'
+image_filename = '/assets/hackcville-logo.png'
 encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 
@@ -52,8 +53,8 @@ encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 #       #'external_url':'https://raw.githubusercontent.com/plotly/dash-app-stylesheets/master/dash-analytics-report.css'
 # })
 
-image_filename = '/Users/jackmcmullen/First_dashboard_stuff/assets/hackcville-logo.png'
-encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+# image_filename = '/Users/jackmcmullen/First_dashboard_stuff/assets/hackcville-logo.png'
+# encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 layout = dict(
     autosize=True,
